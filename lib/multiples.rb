24652c7606927b256c7b48c37af4bffcf46456b7
nikila-saravanan <nikila.saravanan@gmail.com>
2015-10-12 13:20:20 -0400

    done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,13 @@
 # Enter your procedural solution here!
+def collect_multiples(num)
+  range = (1..num-1).to_a
+  multiples = range.select {|item|
+    (item%3 == 0 && item%5 == 0) || item%3 == 0 || item%5 == 0
+  }
+end
+
+def sum_multiples(num)
+  array = collect_multiples(num)
+  sum = 0
+  total = array.inject {|sum,x| sum += x}
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,20 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def initialize(num)
+    @array = (1..num-1).to_a
+  end
+
+  def collect_multiples
+    @array.select {|item|
+      (item%3 == 0 && item%5 == 0) || item%3 == 0 || item%5 == 0
+    }
+  end
+
+  def sum_multiples
+    multiples = collect_multiples
+    sum = 0
+    @total = multiples.inject {|sum,x| sum += x}
+  end
+
+
+end
